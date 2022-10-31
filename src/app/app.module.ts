717b35610f063ac4f77c4bd32a6dd444c8f5416d
AlexDsmr <ai.danilov.smr@gmail.com>
2022-11-01 01:52:15 +0400

    feat: add filter component layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoListPageComponent } from './pages/todo-list-page/todo-list-page.component';
 import { TodoTableComponent } from './components/table/todo-table.component';
+import { FilterTaskComponent } from './components/filter-task/filter-task.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodoListPageComponent,
-    TodoTableComponent
+    TodoTableComponent,
+    FilterTaskComponent
   ],
   imports: [
     BrowserModule,
