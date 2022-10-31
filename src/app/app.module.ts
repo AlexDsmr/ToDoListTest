d06f1c477ce74341096d96a77be0dc16dfd6a5b8
AlexDsmr <ai.danilov.smr@gmail.com>
2022-10-31 21:26:55 +0400

    feat: add basic structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoListPageComponent } from './pages/todo-list-page/todo-list-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListPageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/todo-list-page/todo-list-page.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'app-todo-list-page',
+	templateUrl: './todo-list-page.component.html',
+	styleUrls: ['./todo-list-page.component.scss']
+})
+export class TodoListPageComponent implements OnInit {
+	constructor() {}
+
+	ngOnInit(): void {
+		console.log('123');
+	}
+}
