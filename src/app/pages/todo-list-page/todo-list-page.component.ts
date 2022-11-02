cb57d24b52af563910b0a140e015741fde561fe4
AlexDsmr <ai.danilov.smr@gmail.com>
2022-11-02 20:47:57 +0400

    feat: add onDestroy for todo list page

--- src/app/pages/todo-list-page/todo-list-page.component.ts
+++ src/app/pages/todo-list-page/todo-list-page.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Task } from '@interfaces/task.interface';
+import { Subject, take, takeUntil } from 'rxjs';
 import { TasksService } from 'src/app/services/tasks.service';
 
 @Component({
@@ -7,14 +8,21 @@ import { TasksService } from 'src/app/services/tasks.service';
   templateUrl: './todo-list-page.component.html',
   styleUrls: ['./todo-list-page.component.scss']
 })
-export class TodoListPageComponent implements OnInit {
+export class TodoListPageComponent implements OnInit, OnDestroy {
+  private destroyed$ = new Subject<void>();
   constructor(private taskService: TasksService) {}
+  ngOnDestroy(): void {
+    this.destroyed$.next();
+  }
 
   get tasks(): Task[] {
     return this.taskService.getTasks();
   }
 
   ngOnInit(): void {
-    this.taskService.getAll().subscribe(() => {});
+    this.taskService
+      .getAll()
+      .pipe(take(1), takeUntil(this.destroyed$))
+      .subscribe(() => {});
   }
 }
