1131dbb8909cd7996c232629e10711ffc9323935
AlexDsmr <ai.danilov.smr@gmail.com>
2022-11-02 17:47:19 +0400

    feat: change components main logic

--- src/app/components/table/todo-table.component.ts
+++ src/app/components/table/todo-table.component.ts
@@ -14,7 +14,7 @@ export class TodoTableComponent implements OnInit, OnDestroy {
 
   @Input() tasksProps: Task[] = [];
 
-  editingTasksIds: number[] = [];
+  editingTasks: Task[] = [];
 
   ngOnInit(): void {}
 
@@ -22,30 +22,50 @@ export class TodoTableComponent implements OnInit, OnDestroy {
     this.destroyed$.next();
   }
 
-  addToEdit(id: number): void {
-    this.editingTasksIds.push(id);
+  addToEdit(task: Task): void {
+    this.editingTasks.push(task);
   }
 
-  deleteFromEdit(id: number): void {
-    const index = this.editingTasksIds.indexOf(id);
-    if (index >= 0) this.editingTasksIds.splice(index, 1);
+  deleteFromEdit(task: Task): void {
+    const index = this.editingTasks.indexOf(task);
+    if (index >= 0) this.editingTasks.splice(index, 1);
   }
 
-  isEditingTask(id: number): boolean {
-    return this.editingTasksIds.includes(id);
+  isEditingTask(task: Task): boolean {
+    return this.editingTasks.includes(task);
   }
 
-  changeTaskStatus(id: number, newStatus: boolean): void {
-    this.taskService
-      .changeTaskStatus(id, newStatus)
-      .pipe(take(1), takeUntil(this.destroyed$))
-      .subscribe((task) => {
-        this.tasksProps.map((task) => {
-          if (task.id === task.id) {
-            task.isCompleted = task.isCompleted;
-          }
-        });
+  changeTaskStatus(task: Task): void {
+    if (this.isEditingTask(task)) {
+      this.editingTasks.map((editingTask) => {
+        if (editingTask.id === task.id) {
+          editingTask.isCompleted = !task.isCompleted;
+        }
       });
+    } else {
+      this.taskService
+        .changeTaskStatus(task.id, !task.isCompleted)
+        .pipe(take(1), takeUntil(this.destroyed$))
+        .subscribe((task) => {
+          this.tasksProps.map((task) => {
+            if (task.id === task.id) {
+              task.isCompleted = task.isCompleted;
+            }
+          });
+        });
+    }
+  }
+
+  updateEditingTask(value: string, task: Task) {
+    this.editingTasks.map((editingTask) => {
+      if (editingTask.id === task.id) {
+        editingTask.task = value;
+      }
+    });
+  }
+
+  acceptEdit(task: Task) {
+    console.log(this.editingTasks);
   }
 
   trackByFn(index, item) {
